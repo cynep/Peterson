801181ce386c0ec098d8d9c42896ace12be9a5c8
cynep <kozyaba@gmail.com>
2015-05-23 03:15:40 -0500

    Form Outline

--- Peterson/Form1.cs
+++ Peterson/Form1.cs
@@ -16,5 +16,24 @@ namespace Peterson
         {
             InitializeComponent();//trying push
         }
+
+        private void customerBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customerBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.InvoiceList' table. You can move, or remove it, as needed.
+            this.invoiceListTableAdapter.Fill(this.database1DataSet.InvoiceList);
+            // TODO: This line of code loads data into the 'database1DataSet.Customer' table. You can move, or remove it, as needed.
+            this.customerTableAdapter.Fill(this.database1DataSet.Customer);
+
+        }
+
+       
     }
 }
